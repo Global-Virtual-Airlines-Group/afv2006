9ba2151ef58a20b01b569a7c25e2712f33fcd1cb
Luke Kolin <luke@deltava.org>
2005-09-01 02:15:11 +0000

    Initial addition
    git-svn-id: file:///home/svn/AFV2006@688 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ static/js/common.js
@@ -0,0 +1,174 @@
+
+function getElement(eName)
+{
+return (document.all) ? document.all(eName) : document.getElementById(eName);
+}
+
+function disableButton(btnName)
+{
+var btn = getElement(btnName);
+if (btn) btn.disabled = true;
+return true;
+}
+
+function enableButton(btnName)
+{
+var btn = getElement(btnName);
+if (btn) btn.disabled = false;
+return true;
+}
+
+function checkSubmit()
+{
+if (document.isSubmit) {
+	alert('This page is already being submitted. Please be patient.');
+	return false;
+}
+ 
+return true;
+}
+
+function setSubmit()
+{
+document.isSubmit = true;
+return true;
+}
+
+function cmdPost(url)
+{
+var form = document.forms[0];
+form.action = url;
+ 
+// Execute the form validation - if any
+if (form.onsubmit) {
+	submitOK = form.onsubmit();
+	if (!submitOK)
+		return false;
+}
+  
+setSubmit();
+form.submit();
+return true;
+}
+
+function cmdGet(url)
+{
+setSubmit();
+self.location = '/' + url;
+return true;
+}
+
+
+function validateText(text, min, title)
+{
+if (!text) return true;
+if (text.value.length < min) {
+	alert('Please provide the ' + title + '.');
+	text.focus();
+	return false;
+}
+
+return true;
+}
+
+function validateNumber(text, minValue, title)
+{
+if (!text) return true;
+intValue = parseFloat(text.value);
+if ((text.value.length < 1) || (intValue == Number.NaN)) {
+	alert('Please provide a numeric ' + title + '.');
+	text.focus();
+	return false;
+}
+
+if (intValue < minValue) {
+	alert('The ' + title + ' must be greater than ' + minValue + '.');
+	text.focus();
+	return false;
+}
+
+return true;
+}
+
+function validateCombo(combo, title)
+{
+if (!combo) return true;
+if (combo.selectedIndex == 0) {
+	alert('Please provide the ' + title + '.');
+	combo.focus();
+	return false;
+}
+
+return true;
+}
+
+function validateFile(fileName, extType, title)
+{
+if ((!fileName) || (fileName.value.length == 0)) return true;
+extTypes = extType.toLowerCase().split(',');
+
+fName = fileName.value;
+ext = fName.substring(fName.length - 3, fName.length).toLowerCase();
+for (x = 0; x < extTypes.length; x++) {
+	if (ext == extTypes[x])
+		return true;
+}
+
+alert('The ' + title + ' must be a ' + extType.toUpperCase() + ' file.');
+fileName.focus();
+return false;
+}
+
+function validateCheckBox(checkbox, minSelected, title)
+{
+if (!checkbox) return true;
+var checkCount = 0;
+for (x = 0; x < checkbox.length; x++) {
+	if (checkbox[x].checked)
+		checkCount++;
+}
+
+if (checkCount >= minSelected) return true;
+alert('At least ' + minSelected + ' ' + title + ' must be selected.');
+checkbox[0].focus();
+return false;
+}
+
+function setDaysInMonth(combo)
+{
+var daysInMonth = new Array(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31);
+var month = parseInt(combo.options[combo.selectedIndex].value);
+
+var dCombo = document.forms[0].dateD;
+dCombo.options.length = daysInMonth[month];
+for (x = 1; x <= daysInMonth[month]; x++)
+	dCombo.options[x-1] = new Option(x);
+	
+return true;
+}
+
+function initDateCombos(mCombo, dCombo, d)
+{
+mCombo.selectedIndex = d.getMonth();
+setDaysInMonth(mCombo);
+dCombo.selectedIndex = (d.getDate() - 1);
+return true;
+}
+
+function getXMLHttpRequest()
+{
+var req;
+try {
+	req = new XMLHttpRequest();
+} catch (e) {
+	var MSXML_XMLHTTP_PROGIDS = new Array('MSXML2.XMLHTTP.5.0','MSXML2.XMLHTTP.4.0','MSXML2.XMLHTTP.3.0','MSXML2.XMLHTTP','Microsoft.XMLHTTP');
+	var success = false;
+	for (var i = 0; i < MSXML_XMLHTTP_PROGIDS.length && (!req); i++) {
+		try {
+			req = new ActiveXObject(MSXML_XMLHTTP_PROGIDS[i]);
+		} catch (e) {}
+	}
+}
+
+return req;
+}
---
+++ static/js/googleMaps.js
@@ -0,0 +1,86 @@
+var displayedMarkers = new Array();
+
+function googleMarker(imgPath, color, point, label)
+{
+if (color == 'null')
+	return point;
+
+var icon = new GIcon();
+icon.image = '/' + imgPath + '/maps/point_' + color + '.png';
+icon.shadow = '/' + imgPath + '/maps/shadow.png';
+icon.iconSize = new GSize(12, 20);
+icon.shadowSize = new GSize(22, 20);
+icon.iconAnchor = new GPoint(6, 20);
+icon.infoWindowAnchor = new GPoint(5, 1);
+var marker = new GMarker(point, icon);
+if (label != null)
+	GEvent.addListener(marker, 'click', function() { marker.openInfoWindowHtml(label); });
+	
+return marker;
+}
+
+function addMarkers(map, arrayName)
+{
+// Get the map data
+var markers = eval(arrayName);
+if (!markers) return false;
+
+// Add the map data, either an array or a single element
+if (isNaN(markers.length)) {
+	map.addOverlay(markers);
+} else if (markers.length > 0) {
+	for (x = 0; x < markers.length; x++)
+		map.addOverlay(markers[x]);
+}
+
+displayedMarkers[arrayName] = true;
+return true;
+}
+
+function removeMarkers(map, arrayName)
+{
+// Get the map data
+var markers = eval(arrayName);
+if (!markers) return false;
+
+// Remove the map data, either an array or a single element
+if (isNaN(markers.length)) {
+	map.removeOverlay(markers);
+} else if (markers.length > 0) {
+	for (x = 0; x < markers.length; x++)
+		map.removeOverlay(markers[x]);
+}
+	
+displayedMarkers[arrayName] = false;
+return true;
+}
+
+function getDefaultZoom(distance)
+{
+if (distance > 6100) {
+	return 15;
+} else if (distance > 2900) {
+	return 14;
+} else if (distance > 1600) {
+	return 13;
+} else if (distance > 780) {
+	return 12;
+} else if (distance > 390) {
+	return 11;
+} else if (distance > 195) {
+	return 10;
+} else if (distance > 90) {
+	return 9;
+} else if (distance > 50) {
+	return 8;
+}
+
+return 7;
+}
+
+function toggleMarkers(map, arrayName)
+{
+// Figure out if we add or remove the markers
+var isToggled = displayedMarkers[arrayName];
+return (isToggled) ? removeMarkers(map, arrayName) : addMarkers(map, arrayName);
+}
