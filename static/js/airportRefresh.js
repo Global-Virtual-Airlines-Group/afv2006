0eb08f6f6e36c6c987f257530a4c8ba277dbbe50
Luke Kolin <luke@deltava.org>
2005-09-01 02:15:11 +0000

    Initial addition
    git-svn-id: file:///home/svn/AFV2006@688 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

---
+++ static/js/airportRefresh.js
@@ -0,0 +1,74 @@
+function updateAirports(combo, cmdURL, doICAO, oldCode)
+{
+var xmlreq = getXMLHttpRequest();
+xmlreq.open("GET", "airports.ws?" + cmdURL, true);
+xmlreq.onreadystatechange = function() {
+	if (xmlreq.readyState != 4) return false;
+	var xmlDoc = xmlreq.responseXML;
+	var ac = xmlDoc.documentElement.getElementsByTagName("airport");
+	var codeAttr = (doICAO) ? "icao" : "iata";
+	combo.options.length = ac.length + 1;
+	combo.options[0] = new Option("-", "");
+	for (var i = 0; i < ac.length; i++) {
+		var a = ac[i];
+		var apCode = a.getAttribute(codeAttr);
+		var apName = a.getAttribute("name") + "(" + apCode + ")";
+		combo.options[i+1] = new Option(apName, apCode);
+	} // for
+
+	setAirport(combo, oldCode);
+	changeAirport(combo);
+	combo.disabled = false;
+	return true;
+}
+
+combo.disabled = true;
+xmlreq.send(null);
+return true;
+}
+
+function changeAirline(aCombo)
+{
+var f = document.forms[0];
+updateAirports(f.airportD, 'airline=' + getValue(aCombo), false, getValue(f.airportD));
+updateAirports(f.airportA, 'airline=' + getValue(aCombo), false, getValue(f.airportA));
+return true;
+}
+
+function updateOrigin(combo)
+{
+var f = document.forms[0];
+updateAirports(f.airportA, 'code=' + getValue(combo), false, getValue(f.airportA));
+return true;
+}
+
+function changeAirport(combo)
+{
+var text = document.getElementById(combo.name + 'Code');
+if (text) text.value = combo.options[combo.selectedIndex].value.toUpperCase();
+return true;
+}
+
+function getValue(combo)
+{
+if (combo.selectedIndex == -1)
+	return null;
+	
+return combo.options[combo.selectedIndex].value;
+}
+
+function setAirport(combo, code)
+{
+if (code == null)
+	return false;
+
+code = code.toUpperCase();
+for (x = 0; x < combo.options.length; x++) {
+	if (code == combo.options[x].value) {
+		combo.selectedIndex = x;
+		return true;
+	}
+}
+
+return true;
+}
