9b47af8f15e46aff3d8a1416600eb8d5b0c4672a
Luke Kolin <luke@deltava.org>
2005-09-25 18:43:26 +0000

    Open external links in new window
    git-svn-id: file:///home/svn/AFV2006@1147 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- static/js/common.js
+++ static/js/common.js
@@ -108,7 +108,7 @@ if (lat ==- 1){
 	return false;
 }
 
-if ((str.indexOf('.') < 1) || (str.indexOf('.') == str.length())) {
+if ((str.indexOf('.') < 1) || (str.indexOf('.') == str.length)) {
 	alert('Please provide a valid ' + title + '.');
 	text.focus();
 	return false;
@@ -222,4 +222,17 @@ try {
 }
 
 return req;
+}
+
+function initLinks()
+{
+if (!document.getElementsByTagName) return;
+var anchors = document.getElementsByTagName("a");
+for (var i = 0; i < anchors.length; i++) {
+	var anchor = anchors[i];
+	if (anchor.getAttribute("href") && anchor.getAttribute("rel") == "external")
+		anchor.target = "_blank";
+}
+
+return true;
 }
