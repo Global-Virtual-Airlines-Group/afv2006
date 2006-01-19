6308d478a4b06be4c52d0f292fa4cc9ca9657bb6
Luke Kolin <luke@deltava.org>
2006-01-19 04:14:54 +0000

    Fix JS error
    git-svn-id: file:///home/svn/AFV2006@2090 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- static/js/common.js
+++ static/js/common.js
@@ -174,7 +174,7 @@ return false;
 
 function validateCheckBox(checkbox, minSelected, title)
 {
-if (!checkbox) || return true;
+if (!checkbox) return true;
 var checkCount = 0;
 for (x = 0; x < checkbox.length; x++) {
 	if (checkbox[x].checked)
