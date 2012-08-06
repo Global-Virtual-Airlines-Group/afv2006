c1106dd8759b717d5fe12f8a675ad2fd59f456a2
Luke Kolin <luke@deltava.org>
2012-08-06 01:40:04 +0000

    Adjust updateMapText to golgotha.maps namespace
    git-svn-id: file:///home/svn/AFV2006@8202 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- static/js/googleMapsV3.js
+++ static/js/googleMapsV3.js
@@ -9,7 +9,17 @@ golgotha.maps.z = {INFOWINDOW:100, POLYLINE:50, MARKER:25, OVERLAY:10};
 golgotha.maps.ovLayers = [];
 
 // Set best text color for map types
-golgotha.maps.TextColor = {roadmap:'#002010', satellite:'#efefef', terrain:'#002010'};
+golgotha.maps.TEXT_COLOR = {roadmap:'#002010', satellite:'#efefef', terrain:'#002010'};
+golgotha.maps.updateMapText = function () {
+	var newColor = golgotha.maps.TEXT_COLOR[this.getMapTypeId()];
+	var elements = getElementsByClass('mapTextLabel');
+	for (var x = 0; x < elements.length; x++) {
+		var el = elements[x];
+		el.style.color = newColor;
+	}
+
+	return true;
+}
 
 golgotha.maps.displayedMarkers = [];
 golgotha.maps.setMap = function(map) {
@@ -19,6 +29,7 @@ golgotha.maps.setMap = function(map) {
 		golgotha.maps.displayedMarkers.push(this);
 	
 	this.setMap_OLD(map);
+	return true;
 }
 
 // Track overlays
@@ -201,18 +212,6 @@ if (!check.checked)
 	removeMarkers(arrayName);
 else
 	addMarkers(map, arrayName);
-
-return true;
-}
-
-function updateMapText()
-{
-var newColor = golgotha.maps.TextColor[this.getMapTypeId()];
-var elements = getElementsByClass('mapTextLabel');
-for (var x = 0; x < elements.length; x++) {
-	var el = elements[x];
-	el.style.color = newColor;
-}
 
 return true;
 }
