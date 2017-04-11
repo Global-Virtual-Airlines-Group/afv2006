6a61ebada4c70452eb86711ff2abef7cd82ea305
Luke Kolin <luke@deltava.org>
2017-04-11 14:15:47 +0000

    Mirror with DVA
    git-svn-id: file:///home/svn/AFV2006@11571 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- static/js/acarsFlightMap.js
+++ static/js/acarsFlightMap.js
@@ -1,11 +1,10 @@
-golgotha.maps.acarsFlight = golgotha.maps.acarsFlight || {selectedFIRs:[], routePoints:[], routeMarkers:[]}; 
-
+golgotha.maps.acarsFlight = golgotha.maps.acarsFlight || {selectedFIRs:[], routePoints:[], routeMarkers:[], airspace:[]}; 
+golgotha.maps.acarsFlight.airspaceColors = {'P':{c:'#ee1010',tx:0.4,z:10}, 'R':{c:'#adad10',tx:0.2,z:5}, 'B':{c:'#10e0e0',tx:0.1,z:0}, 'C':{c:'#ffa018', tx:0.125,z:1}, 'D':{c:'#608040', tx:0.175,z:2}};
 golgotha.maps.acarsFlight.getACARSData = function(pirepID, doToggle)
 {
 // Disable checkboxes
 var f = document.forms[0];
-f.showFDR.disabled = true;
-f.showRoute.disabled = true;
+f.showFDR.disabled = true; f.showRoute.disabled = true; f.showAirspace.disabled = true;
 
 // Build the XML Requester
 var xmlreq = new XMLHttpRequest();
@@ -13,7 +12,7 @@ xmlreq.open('GET', 'acars_pirep.ws?id=' + pirepID, true);
 xmlreq.onreadystatechange = function() {
 	if ((xmlreq.readyState != 4) || (xmlreq.status != 200)) return false;
 	var js = JSON.parse(xmlreq.responseText);
-	js.positons.forEach(function(p) {
+	js.positions.forEach(function(p) {
 		var mrk;
 		golgotha.maps.acarsFlight.routePoints.push(p.ll);
 		if (p.icon) {
@@ -37,12 +36,22 @@ xmlreq.onreadystatechange = function() {
 			google.maps.event.addListener(mrk, 'click', golgotha.maps.acarsFlight.hideATC);
 	});
 
+	js.airspace.forEach(function(a) {
+		if (a.exclude) return false;
+		var ac = golgotha.maps.acarsFlight.airspaceColors[a.type];
+		var brd = new google.maps.Polygon({map:map, paths:[a.border], strokeColor:ac.c, strokeWeight:1, strokeOpacity:ac.tx, fillColor:'#802020', fillOpacity:0.1, zIndex:golgotha.maps.z.POLYGON, visible:false});
+		brd.info = a.info; brd.ll = a.ll;
+		google.maps.event.addListener(brd, 'click', function() { map.infoWindow.setContent(this.info); map.infoWindow.open(map); map.infoWindow.setPosition(this.ll); });
+		golgotha.maps.acarsFlight.airspace.push(brd);
+	});
+
 	golgotha.maps.acarsFlight.gRoute = new google.maps.Polyline({path:golgotha.maps.acarsFlight.routePoints, strokeColor:'#4080af', strokeWeight:3, strokeOpacity:0.85, geodesic:true, zIndex:golgotha.maps.z.POLYLINE});
 	golgotha.event.beacon('ACARS', 'Flight Data');
 
 	// Enable checkboxes
 	golgotha.util.disable(f.showFDR, false);
 	golgotha.util.disable(f.showRoute, false);
+	golgotha.util.disable(f.showAirspace, false);
 	if (doToggle) f.showRoute.click();
 	return true;
 };
@@ -67,6 +76,11 @@ golgotha.maps.acarsFlight.showAPP = function(ctr, range) {
 	return true;
 };
 
+golgotha.maps.acarsFlight.toggleAirspace = function(show) {
+	golgotha.maps.acarsFlight.airspace.forEach(function(as) { as.setVisible(show); });
+	return true;
+};
+
 golgotha.maps.acarsFlight.showFIR = function(code)
 {
 var xmlreq = new XMLHttpRequest();
@@ -77,8 +91,7 @@ xmlreq.onreadystatechange = function() {
 	golgotha.maps.acarsFlight.hideATC();
 	js.firs.forEach(function(fe) {
 		if (fe.border.length == 0) return false;
-		fe.border.push(fe.border[0]);
-		golgotha.maps.acarsFlight.selectedFIRs.push(new google.maps.Polygon({map:map, paths:[fe.border], strokeColor:'#efefff', strokeWeight:1, stokeOpacity:0.85, fillColor:'#7f7f80', fillOpacity:0.25, zIndex:golgotha.maps.z.POLYGON}));
+		golgotha.maps.acarsFlight.selectedFIRs.push(new google.maps.Polygon({map:map, paths:[fe.border], strokeColor:'#efefff', strokeWeight:1, strokeOpacity:0.85, fillColor:'#7f7f80', fillOpacity:0.25, zIndex:golgotha.maps.z.POLYGON}));
 	});
 
 	golgotha.event.beacon('ACARS', 'Show FIR', code);
