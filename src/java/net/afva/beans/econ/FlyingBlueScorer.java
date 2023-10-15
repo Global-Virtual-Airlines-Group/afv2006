0f0d001e95f2dccb4e7887a46e340e130d082e4c
Luke Kolin <luke@deltava.org>
2023-10-15 18:14:17 +0000

    Make AuthoredBean
    git-svn-id: file:///home/svn/AFV2006@19311 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/net/afva/beans/econ/FlyingBlueScorer.java
+++ src/java/net/afva/beans/econ/FlyingBlueScorer.java
@@ -8,7 +8,7 @@ import org.deltava.beans.flight.*;
 /**
  * An Elite program scorer for Air France / KLM.
  * @author Luke
- * @version 11.0
+ * @version 11.1
  * @since 11.0  
  */
 
@@ -17,6 +17,7 @@ public class FlyingBlueScorer extends EliteScorer {
 	@Override
 	public FlightEliteScore score(ScorePackage pkg, EliteLevel lvl) {
 		FDRFlightReport ffr = pkg.getFlightReport();
+		_score.setAuthorID(ffr.getAuthorID());
 		
 		// Calculate base miles, break out if needed
 		if (score(ffr, lvl) == null) return null;
@@ -39,11 +40,12 @@ public class FlyingBlueScorer extends EliteScorer {
 	public FlightEliteScore score(FlightReport fr, EliteLevel lvl) {
 		if (!canScore(fr)) return null;
 		reset(fr.getID(), lvl);
+		_score.setAuthorID(fr.getAuthorID());
 		_score.setDistance(fr.getDistance());
 		
 		setBase(fr.getDistance(), fr.hasAttribute(FlightReport.ATTR_ACARS) ? "ACARS/XACARS/simFDR Base Miles" : "Base Miles");
 		
-		addBonus(Math.round(_score.getPoints() * lvl.getBonusFactor()), lvl.getName() + " Supplement", lvl.getBonusFactor() > 0f);
+		addBonus(Math.round(_score.getPoints() * lvl.getBonusFactor()), String.format("% Supplement", lvl.getName()), lvl.getBonusFactor() > 0f);
 		return _score;
 	}
 }
