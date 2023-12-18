3844c0aeab01f3e2e87d9dda16a865602253d614
Luke Kolin <luke@deltava.org>
2023-12-18 19:49:48 +0000

    Sync code with DVA
    git-svn-id: file:///home/svn/AFV2006@19512 1e6d2d28-e1fb-0310-b8ab-81908d8c6b6a

--- src/java/net/afva/beans/econ/FlyingBlueScorer.java
+++ src/java/net/afva/beans/econ/FlyingBlueScorer.java
@@ -4,6 +4,7 @@ package net.afva.beans.econ;
 import org.deltava.beans.acars.RunwayDistance;
 import org.deltava.beans.econ.*;
 import org.deltava.beans.flight.*;
+import org.deltava.util.system.SystemData;
 
 /**
  * An Elite program scorer for Air France / KLM.
@@ -13,11 +14,14 @@ import org.deltava.beans.flight.*;
  */
 
 public class FlyingBlueScorer extends EliteScorer {
+	
+	private static final int MAX_ACCEL_PCT = SystemData.getInt("econ.elite.maxAccelPct", 20);
+	
+	private static final int MIN_DISTANCE = 500;
 
 	@Override
 	public FlightEliteScore score(ScorePackage pkg, EliteLevel lvl) {
 		FDRFlightReport ffr = pkg.getFlightReport();
-		_score.setAuthorID(ffr.getAuthorID());
 		
 		// Calculate base miles, break out if needed
 		if (score(ffr, lvl) == null) return null;
@@ -26,12 +30,24 @@ public class FlyingBlueScorer extends EliteScorer {
 		if (pkg.getRunwayA() instanceof RunwayDistance ra) {
 			double ls = LandingScorer.score(ffr.getLandingVSpeed(), ra.getDistance());
 			LandingRating lr = LandingRating.rate((int)ls);
-			addBonus(125, "Acceptable Landing", (lr == LandingRating.ACCEPTABLE));
-			addBonus(350, "Good Landing", (lr == LandingRating.GOOD));
+			addBonus(Math.round(ffr.getDistance() * 0.15f), "Acceptable Landing", (lr == LandingRating.ACCEPTABLE));
+			addBonus(Math.round(ffr.getDistance() * 0.375f), "Good Landing", (lr == LandingRating.GOOD));
 		}
 
-		
-		// Calculate no acceleration bonus
+		// Calculate minimal acceleration bonus
+		if (ffr instanceof ACARSFlightReport afr) {
+			int accTime = afr.getTime(2) + afr.getTime(4);
+			long accPct = accTime * 100 / afr.getBlockTime().toSeconds();
+			if (accPct < MAX_ACCEL_PCT) {
+				addBonus(ffr.getDistance() / 2, String.format("Minimal Time Acceleration - %d%%", Long.valueOf(accPct)), true);
+				_score.setDistance(Math.max(MIN_DISTANCE, ffr.getDistance()));
+			} else
+				_score.setDistance(Math.max(MIN_DISTANCE, Math.round(ffr.getDistance() * (1f - (accPct / 2)))));
+			
+			// Calculate on-time bonus
+			addBonus(Math.round(ffr.getDistance() * 0.4f), "Early Arrival", (afr.getOnTime() == OnTime.EARLY));
+			addBonus(Math.round(ffr.getDistance() * 0.15f), "On Time Arrival", (afr.getOnTime() == OnTime.ONTIME));
+		}
 		
 		return _score;
 	}
